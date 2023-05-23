353444cde6d907b64713151f1302441964cba47e
Khiem Huynh Trong <ldcc.khiemhuynh@lotte.vn>
2023-05-23 10:24:51 +0700

    Initial Commit

---
+++ HTKLoanCalculator/HTKLoanCalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  HTKLoanCalculatorApp.swift
+//  HTKLoanCalculator
+//
+//  Created by Viet Nguyen on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HTKLoanCalculatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
